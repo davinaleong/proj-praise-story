c1e26cfbd347d4814d811f6297b5d6d939126b4d
Davina Leong <davina.leong.sy@gmail.com>
2025-05-12 20:38:54 +0800

    Done with testimony model

---
+++ app/Models/Testimony.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
+
+class Testimony extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'content',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
