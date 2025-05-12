ce5ce643e63e237ca3728e8d20393f2141c70be4
Davina Leong <davina.leong.sy@gmail.com>
2025-05-12 22:03:51 +0800

    Updated setting helper

--- app/Helpers/Setting.php
+++ app/Helpers/Setting.php
@@ -6,11 +6,13 @@ class Setting
 {
     const ITEMS_PER_PAGE_100 = 100;
     const ITEMS_PER_PAGE_200 = 200;
-    const ITEMS_PER_PAGE_300 = 300;
+    const ITEMS_PER_PAGE_500 = 500;
+    const ITEMS_PER_PAGE_1000 = 1000;
 
     const ITEMS_PER_PAGE = [
         self::ITEMS_PER_PAGE_100,
         self::ITEMS_PER_PAGE_200,
-        self::ITEMS_PER_PAGE_300,
+        self::ITEMS_PER_PAGE_500,
+        self::ITEMS_PER_PAGE_1000,
     ];
 }
