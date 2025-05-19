6df4b691cf016c4e7489acdf8fb9cc42d8ab23c0
Davina Leong <davina.leong.sy@gmail.com>
2025-05-19 22:52:09 +0800

    Done with the index page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
-<body>
+<body class="bg-yellow-50 text-black dark:bg-blue-950 dark:text-white">
     <main class="container mx-auto p-4">
         {{ $slot }}
     </main>
