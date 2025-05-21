a21b7c22289d967460179a81778d8b6bb6b34b7e
Davina Leong <davina.leong.sy@gmail.com>
2025-05-21 17:22:35 +0800

    Removed disabled CTA

--- resources/views/livewire/testimonies/index.blade.php
+++ resources/views/livewire/testimonies/index.blade.php
@@ -19,15 +19,11 @@
                             Everyone can share public praise stories for free. <br class="hidden sm:inline" />
                             Upgrade to premium to keep selected testimonies private — just for you or your inner circle.
                         </p>
-                        {{-- <a href="{{ route('me.login') }}"
+
+                        <a href="{{ route('me.login') }}"
                         class="inline-block bg-white text-black dark:bg-black dark:text-white font-semibold text-sm px-5 py-2 rounded-md transition hover:opacity-90">
                             Learn More & Upgrade
-                        </a> --}}
-
-                        <button type="button" disabled
-                        class="inline-block bg-white text-black dark:bg-black dark:text-white font-semibold text-sm px-5 py-2 rounded-md transition hover:opacity-90">
-                            Coming Soon
-                        </button>
+                        </a>
                     </div>
                 @endif
 
