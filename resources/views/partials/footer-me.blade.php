3ef89181012ffd06eb58f74325a8810c5b7368a8
Davina Leong <davina.leong.sy@gmail.com>
2025-05-21 15:59:44 +0800

    Removed the contact us link

--- resources/views/partials/footer-me.blade.php
+++ resources/views/partials/footer-me.blade.php
@@ -3,8 +3,6 @@
         @include('partials.copyright')
     </p>
     <p class="text-sm text-gray-500 dark:text-gray-400 text-center mt-2">
-        <span><a href="{{ route('me.contact') }}" class="underline hover:text-gray-700 dark:hover:text-gray-300">Contact Us</a></span>
-        <span>|</span>
         <span><a href="{{ route('me.terms-and-conditions') }}" class="underline hover:text-gray-700 dark:hover:text-gray-300">T&amp;C</a></span>
         <span>|</span>
         <span><a href="{{ route('me.privacy-policy') }}" class="underline hover:text-gray-700 dark:hover:text-gray-300">Privacy</a></span>
