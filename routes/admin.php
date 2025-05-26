639b35d907017c960ae740da2295ea4e8cf703da
Davina Leong <davina.leong.sy@gmail.com>
2025-05-26 19:30:23 +0800

    WIP dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LogoutController;
 use App\Livewire\Admin\Auth\Login;
+use App\Livewire\Admin\Dashboard;
 
 $prefix = config('admin.prefix', '/admin');
 
@@ -12,9 +13,10 @@ Route::prefix($prefix)->name('admin.')->group(function () use ($prefix) {
     Route::get('/login', Login::class)->name('login');
 
     Route::middleware('auth:admin')->group(function () {
-        Route::get('/protected-test', function () {
-            return 'You are authenticated as an admin.';
-        })->name('protected.test');
+        // Route::get('/protected-test', function () {
+        //     return 'You are authenticated as an admin.';
+        // })->name('protected.test');
+        Route::get('/dashboard', Dashboard::class)->name('dashboard');
 
         Route::post('/logout', LogoutController::class)->name('logout');
     });
